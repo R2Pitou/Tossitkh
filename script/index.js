81880547c15eb6dafb460e76b8c53e61678649eb
R2P2 <101490847+R2Pitou@users.noreply.github.com>
2024-01-10 08:36:23 +0700

    initial upload
    Fighting with VS code to forget the old Repo with fatal Windows files.

---
+++ script/index.js
@@ -0,0 +1,39 @@
+import { SaladMenu, WrapMenu, mueslisMenu, sidesMenu } from "./menu.js";
+import { formatCurrency } from "./moneyconvert.js";
+
+function renderMenu(menu) {
+  let menuHTML = "";
+
+  menu.forEach((item) => {
+    menuHTML += `
+      <div class="col item-x">
+        <div>
+          <img src="${item.img}" width="100px" />
+        </div>
+        <div>
+          <p class="menu-name-x">${item.name}</p>
+          <p class="menu-price-x">$${formatCurrency(item.priceCents)}</p>
+        </div>
+      </div>`;
+  });
+
+  document.querySelector(".menu-container-js").innerHTML = menuHTML;
+}
+
+document.querySelector(".salad-js").addEventListener("click", () => {
+  renderMenu(SaladMenu);
+});
+
+document.querySelector(".wrap-js").addEventListener("click", () => {
+  renderMenu(WrapMenu);
+});
+
+document.querySelector(".muesli-js").addEventListener("click", () => {
+  renderMenu(mueslisMenu);
+});
+
+document.querySelector(".side-js").addEventListener("click", () => {
+  renderMenu(sidesMenu);
+});
+
+renderMenu(SaladMenu);
